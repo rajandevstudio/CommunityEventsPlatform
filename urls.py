da90b67f8d57be62da9f84c6783f37fe1bd484c8
Rajan <bagoriarajan@gmail.com>
2026-01-19 23:50:39 +0530

    fix: Refactor base.html for improved structure and readability; update urls.py for static file handling

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
-
+from django.conf.urls.static import static
 import settings
 ok = JsonResponse({"ok": True})
 urlpatterns = [
@@ -23,3 +23,8 @@ if settings.DEBUG:
         path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
         path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema')),
     ]
+
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT if hasattr(settings, "STATIC_ROOT") else None
+    )
