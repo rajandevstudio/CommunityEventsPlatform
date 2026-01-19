8d82a257305d151024a21b7b4363203075446b1c
Rajan <bagoriarajan@gmail.com>
2026-01-19 23:06:35 +0530

    fix: Update DEBUG setting to use environment variable for production safety

--- settings.py
+++ settings.py
@@ -59,8 +59,7 @@ LOGGING = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.getenv("DEBUG").lower() == 'true'
-DEBUG = True
+DEBUG = os.getenv("DEBUG").lower() == 'true'
 
 ALLOWED_HOSTS = ["*"]
 
