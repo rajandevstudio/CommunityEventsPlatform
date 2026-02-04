cf8c127eba2ccb2af54f0a103a80af37111ec539
Rajan <bagoriarajan@gmail.com>
2026-02-04 23:12:32 +0530

    static file served via whitenoise

--- settings.py
+++ settings.py
@@ -127,6 +127,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -218,8 +219,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / "static"]
+
 
 
 
--- web_server.sh
+++ web_server.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Running database migrations..."
-python manage.py migrate --noinput
+# echo "Running database migrations..."
+# python manage.py migrate --noinput
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
@@ -10,6 +10,6 @@ python manage.py collectstatic --noinput
 echo "Starting Gunicorn..."
 gunicorn wsgi:application \
   --bind 0.0.0.0:${PORT:-8080} \
-  --workers 2 \
-  --threads 4 \
+  --workers 1 \
+  --threads 2 \
   --timeout 120
