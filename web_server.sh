f25edb4306ee7dbeb1145a83b07cddd668587e92
Rajan <bagoriarajan@gmail.com>
2026-04-04 13:23:54 +0530

    added fix for migration issue'

--- web_server.sh
+++ web_server.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-# echo "Running database migrations..."
-# python manage.py migrate --noinput
+echo "Running database migrations..."
+python manage.py migrate --noinput
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
